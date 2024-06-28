d4c2bac83ca940d9d881d19a2149a0c2c92a9f96
yashwanth1239 <yashwanthkumarvasireddy@gmail.com>
2024-06-28 23:54:28 +0530

    retrofit instance creation and news api key activation

---
+++ app/src/main/java/com/example/newsapp/util/constants.kt
@@ -0,0 +1,10 @@
+package com.example.newsapp.util
+
+class constants {
+    companion object{
+        const val API_KEY = "003eda9100204257a8da24956af0af7a"
+        const val BASE_URL = "https://newsapi.org/"
+        const val SEARCH_NEWS_TIME_DELAY = "500L"
+        const val QUERY_PAGE_SIZE = "20"
+    }
+}

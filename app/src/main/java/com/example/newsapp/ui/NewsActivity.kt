d8c87d583b704b1e8bffa30656eb7dd0b6ec1e64
yashwanth1239 <yashwanthkumarvasireddy@gmail.com>
2024-06-28 21:28:52 +0530

    coding for all xml files and provided internet access

--- app/src/main/java/com/example/newsapp/ui/NewsActivity.kt
+++ app/src/main/java/com/example/newsapp/ui/NewsActivity.kt
@@ -5,12 +5,13 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.newsprojectpractice.R
 
 class NewsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_news)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)

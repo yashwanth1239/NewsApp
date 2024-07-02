ef014eda45b69148aa89aff9183b65ad0d5998f3
yashwanth1239 <yashwanthkumarvasireddy@gmail.com>
2024-07-02 15:58:03 +0530

    article fragment code completes

--- app/src/main/java/com/example/newsapp/ui/fragments/HeadlinesFragment.kt
+++ app/src/main/java/com/example/newsapp/ui/fragments/HeadlinesFragment.kt
@@ -10,14 +10,4 @@ import com.example.newsprojectpractice.R
 
 class HeadlinesFragment : Fragment() {
 
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_headlines, container, false)
-    }
-
-
 }
